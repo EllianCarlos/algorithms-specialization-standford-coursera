cd3220cf8d0fdb2ce0e5d5df2fc64f9b38e6c75a
Ellian Carlos <elliancarlos@gmail.com>
2024-12-26 04:34:26 +0000

    fix quicksort version

--- src/quicksort.py
+++ src/quicksort.py
@@ -5,17 +5,17 @@ def solve(a: list[int], pivotStrat):
     if pivotStrat == 'f':
       return
     if pivotStrat == 'l':
-      a[l], a[r-1] = a[r-1], a[l]
+      a[l], a[r] = a[r], a[l]
       return
 
-    mid = ((r-1-l) // 2) + l
-    tmp = [a[l],a[r-1],a[mid]]
+    mid = ((r-l) // 2) + l
+    tmp = [a[l],a[r],a[mid]]
     tmp.sort()
 
     if tmp[1] == a[l]:
       return
-    elif tmp[1] == a[r-1]:
-      a[l], a[r-1] = a[r-1], a[l]
+    elif tmp[1] == a[r]:
+      a[l], a[r] = a[r], a[l]
       return
     else:
       a[l], a[mid] = a[mid], a[l]
@@ -25,7 +25,7 @@ def solve(a: list[int], pivotStrat):
   def partition(a: list[int], l: int, r: int) -> int:  
     pivot = a[l]
     j = l + 1
-    for i in range(l+1, r):
+    for i in range(l+1, r+1):
       if a[i] < pivot:
         a[j], a[i] = a[i], a[j]
         j += 1
@@ -33,62 +33,18 @@ def solve(a: list[int], pivotStrat):
     
     return j - 1
 
-  def quicksort(a: list[int], l: int, r: int, totalCmps, pivotStrat) -> list[int]:
-    totalCmps[0] += (r-l-1)
-    if r-1 <= l:
+  def quicksort(a: list[int], l: int, r: int, totalCmps, pivotStrat):
+    if r <= l:
       return a
-    else:
-      choosePivot(a, l, r, pivotStrat)
-      p = partition(a,l,r)
-      quicksort(a,l,p - 1,totalCmps,pivotStrat)
-      quicksort(a,p + 1,r,totalCmps,pivotStrat)
-    return a
 
-  quicksort(a, 0, len(a),totalCmps, pivotStrat)
-  
+    totalCmps[0] += (r-l)
+    choosePivot(a, l, r, pivotStrat)
+    p = partition(a,l,r)
+    quicksort(a,l,p - 1,totalCmps,pivotStrat)
+    quicksort(a,p + 1,r,totalCmps,pivotStrat)
+
+  quicksort(a, 0, len(a) - 1,totalCmps, pivotStrat)
   return totalCmps
-
-def choosePivot(alist,first,last,pivotID):
-    if pivotID == 'first':
-        pass
-
-    if pivotID == 'last':
-        (alist[first], alist[last]) = (alist[last], alist[first])
-
-    elif pivotID == 'middle':
-        mid = (last-first)//2 + first
-        listTemp = [alist[first], alist[last], alist[mid]]
-        listTemp.sort()
-        if listTemp[1] == alist[first]:
-            pivotIndex = first
-        elif listTemp[1] == alist[last]:
-            pivotIndex = last
-        else:
-            pivotIndex = mid
-        (alist[first], alist[pivotIndex]) = (alist[pivotIndex], alist[first])
-
-def partition(alist, first, last):
-    pivotVal = alist[first] # initialise pivot as the first element
-    leftmark = first+1
-    for rightmark in range(first+1,last+1):
-        if alist[rightmark] < pivotVal:
-            (alist[leftmark],alist[rightmark]) = (alist[rightmark],alist[leftmark])
-            leftmark = leftmark + 1
-    (alist[leftmark-1],alist[first]) = (alist[first],alist[leftmark-1])
-
-    return leftmark-1       # partition point := where pivot finally occupies
-
-def quickSort(alist,first,last,pivotID):
-    numComp = last -first
-    if last <= first:
-        return (alist, 0)
-    else:
-        choosePivot(alist,first,last,pivotID)
-        splitpoint = partition(alist,first,last)
-        (Lsorted,numCompL) = quickSort(alist, first, splitpoint-1, pivotID)
-        (Rsorted,numCompR) = quickSort(alist, splitpoint+1, last, pivotID)
-        numComp =  numComp + numCompL + numCompR
-    return (alist, numComp)
 
 if __name__ == "__main__":
   arr = [3,2,1]
@@ -104,7 +60,3 @@ if __name__ == "__main__":
     for strat in strats:
       totalCmps = solve(arr.copy(), strat)
       print(f"number of comparisons: {totalCmps}")
-      ### Correct Numbers:
-      ### 162085 first as pivot
-      ### 138382 middle (median as pivot)
-      ### 164123 last as pivot
